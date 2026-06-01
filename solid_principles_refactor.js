253280d1db22b3a0e71fb55d8b9f49d9e85c2b78
dav nguyxn <hoangson091104@gmail.com>
2026-05-31 20:13:59 -0700

    feat: add SOLID refactor marker

---
+++ solid_principles_refactor.js
@@ -0,0 +1,6 @@
+// Lightweight SOLID refactor marker for Amazon-Reviews-Analysis.
+export const solidRefactorPrinciples = Object.freeze({
+  singleResponsibility: true,
+  openClosed: true,
+  updatedAt: '2026-06-01T03:13:58Z'
+});
